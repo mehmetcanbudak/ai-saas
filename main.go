b6ce1fe0bebf7fcaf239dddbaff7418d5f75412e
Mehmmetcan Budak <mehmetcanbudak@gmail.com>
2024-02-19 13:12:26 +0300

    Added makefile and changed main.go.

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := initEverything(); err != nil {
+		log.Fatal(err)
+	}
+	router := chi.NewMux()
+
+	// router.Get("/", )
+	port := os.Getenv("HTTP_LISTEN_ADDR")
+	slog.Info("Application is running on port: ", "port", port)
+	log.Fatal(http.ListenAndServe(os.Getenv(port), router))
+
+}
+
+func initEverything() error {
+	// if err := godotenv.Load; err != nil {
+	// 	return err
+	// }
+	// return nil
+	return godotenv.Load()
+}
